ef618fda728458671683915ddd07c3cb206e9783
RavaAchmad <ravabbok@gmail.com>
2026-08-19 06:51:38 +0700

    nganu

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import axios from 'axios';
-import * as cheerio from 'cheerio';
+import cheerio from 'cheerio';
 import moment from 'moment-timezone';
 import FormData from "form-data";
 
