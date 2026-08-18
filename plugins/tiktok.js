8d5e1afebf6f6ae2352b6200e2547c639c32784c
RavaAchmad <ravabbok@gmail.com>
2026-08-19 06:56:42 +0700

    fix: update cheerio import to named namespace
    Change `import cheerio from 'cheerio'` to `import * as cheerio from 'cheerio'` to ensure compatibility with ESM module resolution in `plugins/tiktok.js`.

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import moment from 'moment-timezone';
 import FormData from "form-data";
 
